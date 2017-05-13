c8e930dd6bf8ae39f0ab388fa8320cd17234f31c
Bobby Lin <mr.linjiahao@gmail.com>
2017-05-13 17:04:02 +0800

    Added maintenance page (testing with Express Middleware).

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -4,25 +4,53 @@
 
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 let app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', ()=> {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('capitalizeText', (text) => {
+    return text.toUpperCase();
+});
+
 app.set('view engine', 'hbs');
+
+// Logging
+app.use((req, res, next) => {
+   let now = new Date().toString();
+   let log = `${now}: ${req.method} ${req.url}`;
+   console.log(log);
+   fs.appendFile('server.log', log+'\n', (err) => {
+       if (err) {
+           console.log('Unable to append to log.')
+       }
+   });
+   next();
+});
+
+//Maintenance Middle-ware
+app.use((req, res) => {
+   res.render('maintenance.hbs', {
+       pageTitle: 'Maintenance Page',
+       maintenanceMessage: 'Website under maintenance. Will be right back.'
+   })
+});
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (request, response) => {
     response.render('home.hbs', {
         pageTitle: 'Home Page',
-        welcomeMessage: 'Welcome to my Home Page',
-        currentYear: new Date().getFullYear()
+        welcomeMessage: 'Welcome to my Home Page'
     })
 });
 
 app.get('/about', (req, res) => {
     res.render('about.hbs',{
-        pageTitle: 'About Page',
-        currentYear: new Date().getFullYear()
+        pageTitle: 'About Page'
     });
 });
 
